048cc0f7474582eba801d917f5b5df10dc01d2b8
karishmabisht14 <karishma.qt14@gmail.com>
2023-02-08 18:16:09 +0530

    adding styles

---
+++ Task 3/main.js
@@ -0,0 +1,27 @@
+// console.dir(document);
+
+// console.log(document.URL);
+// console.log(document.title);
+document.title = 'Vanilla Script';
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+const headerTitle = document.getElementById('header-title');
+// headerTitle.textContent = "List";
+// headerTitle.innerText = "List1";
+// headerTitle.innerHTML = '<h3>List3</h3>';
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// console.log(headerTitle.innerHTML);
+
+headerTitle.style.borderBottom = '3px solid #000';
+
+const addItems = document.getElementById('add');
+addItems.style.fontWeight = 'bold';
+addItems.style.color = 'green';
+
+
