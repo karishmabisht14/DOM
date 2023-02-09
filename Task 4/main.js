173f3684cd55e48af2e13957ef4958b4b7f90fc4
karishmabisht14 <karishma.qt14@gmail.com>
2023-02-09 10:46:05 +0530

    Background and font Weight change

---
+++ Task 4/main.js
@@ -0,0 +1,10 @@
+const items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+
+items[2].style.backgroundColor = 'green';
+
+for(let i = 0; i < items.length; i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
